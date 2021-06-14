7e34a24b2d314f9027ac4cafa4aa7f2da4df13ec
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2021-06-14 17:12:04 +0100

    Adding an oembed endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :three_d_files
    root "three_d_files#index"
 
+   get 'oembed/:id' , to: 'oembed#show', defaults: { format: 'json' }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
